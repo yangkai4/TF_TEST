a1194941bfed87aded72d3e9aa5b44487d14a1d8
yangkai4 <yangk4@science.regn.net>
2022-12-12 17:51:43 +0800

    aa
    aa

---
+++ modules/ram-user/main.tf
@@ -0,0 +1,47 @@
+provider "alicloud" {
+}
+
+resource "random_uuid" "this" {
+}
+
+locals {
+  name = var.name != "" ? var.name : substr("ram-user-${replace(random_uuid.this.result, "-", "")}", 0, 32)
+}
+
+# RAM user
+resource "alicloud_ram_user" "this" {
+  count = var.create_user ? 1 : 0
+
+  name         = local.name
+  display_name = var.display_name != "" ? var.display_name : null
+  mobile       = var.mobile != "" ? var.mobile : null
+  email        = var.email != "" ? var.email : null
+  force        = var.force
+  comments     = var.comments != "" ? var.comments : null
+}
+
+# RAM login profile
+resource "alicloud_ram_login_profile" "this" {
+  count = var.create_user && var.create_ram_user_login_profile ? 1 : 0
+
+  password                = var.password
+  user_name               = alicloud_ram_user.this[count.index].name
+  password_reset_required = var.password_reset_required
+  mfa_bind_required       = var.mfa_bind_required
+}
+
+# RAM access key
+resource "alicloud_ram_access_key" "this" {
+  count = var.create_user && var.create_ram_access_key ? 1 : 0
+
+  secret_file = var.secret_file != "" ? var.secret_file : null
+  status      = var.status
+  pgp_key     = var.pgp_key != "" ? var.pgp_key : null
+}
+
+resource "alicloud_ram_access_key" "this_no_pgp" {
+  count = var.create_user && var.create_ram_access_key ? 1 : 0
+
+  secret_file = var.secret_file != "" ? var.secret_file : null
+  status      = var.status
+}
---
+++ modules/ram-user/outputs.tf
@@ -0,0 +1,9 @@
+output "this_ram_user_name" {
+  description = "The user's name"
+  value       = module.ram_user.this_ram_user_name
+}
+
+output "this_ram_user_unique_id" {
+  description = "The unique ID assigned by Alibaba Cloud"
+  value       = module.ram_user.this_ram_user_unique_id
+}
